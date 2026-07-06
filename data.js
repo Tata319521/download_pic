573e23dcea925be3060fdde9843dfa5e8a86af93
Tata319521 <1562498381@qq.com>
2026-07-06 23:49:27 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,43 @@
+/*
+  公开展示的设卡资料写在这里。
+
+  添加方法：
+  1. 把高清图片放进 images 文件夹，例如 images/my-oc.png
+  2. 复制一组对象并修改 id、name、category、owner、description、image
+  3. category 只能填写 player / boss / npc
+*/
+
+window.PUBLIC_CARD_DATA = [
+  {
+    id: "public-player-001",
+    name: "玩家设卡展示1",
+    category: "player",
+    owner: "黎隐",
+    description: "用于演示玩家 OC 设卡的展示样式。",
+    image: "./images/pic1.jpg"
+  },
+  {
+    id: "public-player-002",
+    name: "玩家设卡展示2",
+    category: "player",
+    owner: "黎隐",
+    description: "用于演示玩家 OC 设卡的展示样式。",
+    image: "./images/pic2.jpg"
+  },
+  {
+    id: "public-boss-001",
+    name: "boss设卡展示",
+    category: "boss",
+    owner: "黎隐",
+    description: "用于演示 BOSS 设卡的展示样式。",
+    image: "./images/pic3.jpg"
+  },
+  {
+    id: "public-npc-001",
+    name: "npc设卡展示",
+    category: "npc",
+    owner: "黎隐",
+    description: "用于演示 NPC 设卡的展示样式。",
+    image: "./images/player-example.svg"
+  }
+];
